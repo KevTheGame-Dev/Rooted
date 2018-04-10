8a17e7f3ba063ca9ae81f03b1444880c2d2a99f1
MAIN\nch8501 <nch8501@g.rit.edu>
2018-04-10 16:29:58 -0400

    Items can be kind of collected and rotate.

---
+++ Rooted/Assets/ItemScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.rotation = Quaternion.Euler(transform.rotation.x + 10.0f * Time.deltaTime, transform.rotation.y + 10.0f, transform.rotation.z + 10.0f * Time.deltaTime);
+        transform.rotation = Quaternion.Euler(0, Time.time*360, 0);
+        Debug.Log(Time.time);
+
+		
+	}
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Player") 
+            Destroy(gameObject);
+    }
+}
