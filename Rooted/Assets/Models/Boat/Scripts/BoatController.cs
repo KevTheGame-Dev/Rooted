f4dfd22c6d8ba70698cd6a651fb618105c1699ec
Kevin Barrett <kevincb45@gmail.com>
2018-05-06 23:06:31 -0400

    Fixed boat rigidbody issue
    Fixed the issue of the boat not having a rigid body and some other stuff

--- Rooted/Assets/Models/Boat/Scripts/BoatController.cs
+++ Rooted/Assets/Models/Boat/Scripts/BoatController.cs
@@ -11,7 +11,7 @@ public class BoatController : MonoBehaviour {
     //Boat transform
     Transform boatTransform;
     //Offset to keep boat pushed above water, rather than on the same plane as it.
-    float floatOffset = 1.7f;
+    float floatOffset = 4.7f;
 
 	// Use this for initialization
 	void Start () {
@@ -20,6 +20,8 @@ public class BoatController : MonoBehaviour {
 
         boatTransform = this.GetComponent<Transform>();
         this.GetComponent<Rigidbody>().freezeRotation = true;
+        this.GetComponent<Rigidbody>().useGravity = false;
+        
 	}
 	
 	// Update is called once per frame
@@ -29,39 +31,40 @@ public class BoatController : MonoBehaviour {
 
         if (boatTransform.position.y < oceanLevel + floatOffset)
         {
-            float baseForce = Physics.gravity.magnitude;
+            this.GetComponent<Rigidbody>().AddForce(new Vector3(0, 0.1f, 0));
+            //float baseForce = Physics.gravity.magnitude;
 
-            if (Mathf.Abs(boatTransform.position.y - oceanLevel + floatOffset) <= 0.01f)
-            {
-                Debug.Log("Applying weak float force");
-                Vector3 pushUp = Vector3.up * (baseForce + 4);
-                this.GetComponent<Rigidbody>().AddForce(pushUp);
-            }
-            else if (Mathf.Abs(boatTransform.position.y - oceanLevel + floatOffset) <= 0.05f)
-            {
-                Debug.Log("Applying normal float force");
-                Vector3 pushUp = Vector3.up * (baseForce + 9);
-                this.GetComponent<Rigidbody>().AddForce(pushUp);
-            }
-            else if (Mathf.Abs(boatTransform.position.y - oceanLevel + floatOffset) > 0.1f)
-            {
-                Debug.Log("Applying strong float force");
-                Vector3 pushUp = Vector3.up * (baseForce + 30);
-                this.GetComponent<Rigidbody>().AddForce(pushUp);
-            }
+            //if (Mathf.Abs(boatTransform.position.y - oceanLevel + floatOffset) <= 0.01f)
+            //{
+            //    Debug.Log("Applying weak float force");
+            //    Vector3 pushUp = Vector3.up * (baseForce + 4);
+            //    this.GetComponent<Rigidbody>().AddForce(pushUp);
+            //}
+            //else if (Mathf.Abs(boatTransform.position.y - oceanLevel + floatOffset) <= 0.05f)
+            //{
+            //    Debug.Log("Applying normal float force");
+            //    Vector3 pushUp = Vector3.up * (baseForce + 9);
+            //    this.GetComponent<Rigidbody>().AddForce(pushUp);
+            //}
+            //else if (Mathf.Abs(boatTransform.position.y - oceanLevel + floatOffset) > 0.1f)
+            //{
+            //    Debug.Log("Applying strong float force");
+            //    Vector3 pushUp = Vector3.up * (baseForce + 30);
+            //    this.GetComponent<Rigidbody>().AddForce(pushUp);
+            //}
         }
-        if (boatTransform.position.y > oceanLevel + floatOffset + 0.3f)
-        {
-            Vector3 newVelocity = this.GetComponent<Rigidbody>().velocity;
-            if (newVelocity.y > 0)
-            {
-                newVelocity.y = 0;
-            }
-            this.GetComponent<Rigidbody>().velocity = newVelocity;
+        //if (boatTransform.position.y > oceanLevel + floatOffset + 0.3f)
+        //{
+        //    Vector3 newVelocity = this.GetComponent<Rigidbody>().velocity;
+        //    if (newVelocity.y > 0)
+        //    {
+        //        newVelocity.y = 0;
+        //    }
+        //    this.GetComponent<Rigidbody>().velocity = newVelocity;
 
-            //Vector3 newAngVelocity = this.GetComponent<Rigidbody>().angularVelocity;
-            //newAngVelocity.y = 0;
-            //this.GetComponent<Rigidbody>().angularVelocity = newAngVelocity;
-        }
+        //    //Vector3 newAngVelocity = this.GetComponent<Rigidbody>().angularVelocity;
+        //    //newAngVelocity.y = 0;
+        //    //this.GetComponent<Rigidbody>().angularVelocity = newAngVelocity;
+        //}
 	}
 }
