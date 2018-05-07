29bb200cfb763c8e5b6f05f429c8a3fe9aff0910
Kevin Barrett <kevincb45@gmail.com>
2018-05-07 17:49:01 -0400

    Day/Night partially done
    Day night sort of implemented, but broken as shit in terms of the math

--- Rooted/Assets/Scripts/DayNightScript.cs
+++ Rooted/Assets/Scripts/DayNightScript.cs
@@ -6,17 +6,27 @@ public class DayNightScript : MonoBehaviour {
 
     GameObject sun;
     GameObject moon;
+    GameObject moonSphere;
     public float rotateSpeed = 0.001f;
 
 	// Use this for initialization
 	void Start () {
         this.sun = this.transform.GetChild(0).gameObject;
         this.moon = this.transform.GetChild(1).gameObject;
-	}
+        this.moonSphere = this.transform.GetChild(2).gameObject;
+        float moonLight = moon.transform.rotation.x;
+        moonSphere.transform.position = new Vector3(0, Mathf.Sin(moonLight) * 200, Mathf.Cos(moonLight) * 200);
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
+        //rotate directional lights
         sun.transform.Rotate(new Vector3(rotateSpeed, 0, 0));
         moon.transform.Rotate(new Vector3(rotateSpeed, 0, 0));
+
+        //rotate physical moon in sky
+        float moonLight = moon.transform.rotation.x + 100000;
+        moonSphere.transform.position = new Vector3(0, Mathf.Sin(moonLight) * 200, Mathf.Cos(moonLight) * 200);
 	}
 }
