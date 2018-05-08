9cf0ffb29efa3c3c3db72700462937118aabd978
nch8501 <nch8501@rit.edu>
2018-05-07 21:57:02 -0400

    Added the house to the scene, started working on text when player gets to the house

---
+++ Rooted/Assets/Scripts/HouseScript.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HouseScript : MonoBehaviour {
+
+    public GameObject houseClosed;
+    public GameObject houseOpened;
+    Text textField;
+
+    bool beginningDialogue;
+    bool firstList; //if the player has yet to collect all the original objects 
+    bool secondList;
+
+    float time;
+
+
+	// Use this for initialization
+	void Start () {
+        textField = GameObject.FindGameObjectWithTag("Message").GetComponent<Text>();
+        time = 0.0f;
+        beginningDialogue = true;
+        firstList = false;
+        secondList = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //displays text when close to house
+    void DisplayText()
+    {
+        if(beginningDialogue)
+        {
+            
+        }
+        else if(firstList)
+        {
+
+        }
+        else if(secondList)
+        {
+
+        }
+        else
+        {
+
+        }
+
+    }
+
+    //increases time
+    void UpdateTime()
+    {
+        time += Time.deltaTime;
+    }
+
+
+}
