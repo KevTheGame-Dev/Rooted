ff85d9b85160dde9b549281dbbb2b73fc0597ed5
BEN\Ben <btf6119@g.rit.edu>
2018-05-08 00:11:42 -0400

    Background to Text

---
+++ Rooted/Assets/Scripts/TextEmptyChecker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextEmptyChecker : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject PANEL;
+    [SerializeField]
+    private GameObject TEXT;
+
+    private Text textbox;
+
+	// Use this for initialization
+	void Start () {
+        textbox = TEXT.GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        PANEL.SetActive(textbox.text != "");
+	}
+}
